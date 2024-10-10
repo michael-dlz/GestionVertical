2a9e5fa63e0b321b69dbdabf870b93263d12cad0
michael-dlz <105244377+michael-dlz@users.noreply.github.com>
2024-10-10 17:53:52 -0500

    update

--- src/components/Home/Reviews.tsx
+++ src/components/Home/Reviews.tsx
@@ -6,6 +6,8 @@ import "swiper/css";
 
 import "../../index.css"
 
+import { Autoplay } from "swiper/modules";
+
 export function Reviews() {
 
     const { data3 } = useCards()
@@ -16,11 +18,17 @@ export function Reviews() {
                 <div className="w-[1570px] m-auto">
                     <h1 className="font-bold text-4xl text-primary text-center">Usuarios <span className="text-secondary underline">Satisfechos</span></h1>
                     <Swiper
-                        slidesPerView={3}
+                        slidesPerView={3.5}
                         spaceBetween={30}
+                        grabCursor={true}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: true,
+                        }}
                         pagination={{
                             clickable: true,
                         }}
+                        modules={[Autoplay]}
                         className="MySwiper2 w-full flex gap-5 mb-40 mt-24">
                         {data3.map((cardreviews) => (
                             <SwiperSlide>
