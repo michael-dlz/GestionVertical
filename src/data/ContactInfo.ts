18a6d21576875272a5cb93f2fc26a3df49810251
michael-dlz <105244377+michael-dlz@users.noreply.github.com>
2024-10-18 12:16:40 -0500

    update

--- src/data/ContactInfo.ts
+++ src/data/ContactInfo.ts
@@ -9,7 +9,7 @@ export const phone = "+51977788810";
 export const adress = "Urb. Cerros de Camacho, Santiago de Surco, Lima, Perú";
 export const schedule = ["Lunes a Viernes: 9:00 AM - 6:00 PM", "Sábados: 9:00 AM - 2:00 PM"];
 export const socials = [
-    { name: "@Facebook", link: "https://facebook.com/gestionvertical", Icon: IconFacebook },
+    { name: "@Facebook", link: "https://web.facebook.com/GestionVerticalLima", Icon: IconFacebook },
     { name: "@LinkedIn", link: "https://linkedin.com/gestionvertical", Icon: IconLinkedin },
     { name: "@Instagram", link: "https://instagram.com/gestionvertical", Icon: IconInstagram},
 ];
